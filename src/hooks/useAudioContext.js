b2360ad40c23606d9f4d161c64504ed21f1cd121
John Roberts <john.calvin.roberts@gmail.com>
2020-04-19 19:48:23 +0800

    parse id3

--- src/hooks/useAudioContext.js
+++ src/hooks/useAudioContext.js
@@ -1,4 +1,36 @@
 import { useMemo, useCallback } from 'react';
+import MP3Tag from 'mp3tag.js';
+
+const readTags = async (buff) => {
+  let tags;
+  try {
+    const mp3tag = new MP3Tag(buff);
+    const tagger = mp3tag.read();
+    if (tagger.name === 'ID3v2') {
+      tags = tagger.getFrames();
+    }
+  } catch (error) {
+    tags = {};
+  }
+
+  return tags;
+};
+
+const writeTags = async (buff, tags) => {
+  let ret;
+  try {
+    const mp3tag = new MP3Tag(buff);
+    const tagger = mp3tag.read();
+    console.log({ tags });
+    // eslint-disable-next-line no-restricted-syntax
+    for (const tag of tags) {
+      tagger.addFrame(tag);
+    }
+    ret = tagger.getAudio();
+  } catch (error) {
+    ret = buff;
+  }
+};
 
 const initAudioCtx = () =>
   new (window.AudioContext || window.webkitAudioContext)();
@@ -9,10 +41,12 @@ export default () => {
   const decodeAudioData = useCallback(
     async (file) => {
       const audioData = await file.arrayBuffer();
+      const tags = await readTags(audioData);
       // converts the array buffer to an audio buffer
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/AudioBuffer
       // also see: https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/decodeAudioData
       const rawAudioBuffer = await audioContext.decodeAudioData(audioData);
+      // get the right and left channel and meta info from the audio buffer
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/AudioBuffer
       const left = rawAudioBuffer.getChannelData(0);
       const right = rawAudioBuffer.getChannelData(1);
       const meta = {
@@ -20,7 +54,7 @@ export default () => {
         length: rawAudioBuffer.length,
         sampleRate: rawAudioBuffer.sampleRate,
       };
-      return [left, right, meta];
+      return [left, right, meta, tags];
     },
     [audioContext],
   );
