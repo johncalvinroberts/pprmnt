69f577c126ad34520995d37cdc53a5bf51ebb2ea
John Roberts <john.calvin.roberts@gmail.com>
2020-05-03 14:22:23 +0800

    refactor: remove dead id3 code

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,8 +5,6 @@ export { default as isObject } from './isObject';
 export { default as isNumber } from './isNumber';
 export { default as typedArrayToBuffer } from './typedArrayToBuffer';
 export { default as stripFileExtension } from './stripFileExtension';
-export { default as readTags } from './readTags';
-export { default as writeTags } from './writeTags';
 export { default as fileToArrayBuffer } from './fileToArrayBuffer';
 export { default as generateUuid } from './generateUuid';
 export { default as truncateText } from './truncateText';
